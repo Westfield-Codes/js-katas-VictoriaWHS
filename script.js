41fa2d08f1e9d011c7e72321624e79046a4cace3
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-02 18:04:31 +0000

    done

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var petname= prompt ("What is their name?");
 // alert "You have a pet type named pet name 
 alert("You have a " + pettype + " nammed " + petname);
 // if pet is a dog, say "I like dogs, too!"
-if (pettype == "Dog" | "dog") alert("I like dogs but they keep stealing bones from the mysterious bone pit");
+if (pettype == "Dog" || "dog") alert("I like dogs but they keep stealing bones from the mysterious bone pit");
 // if it is a cat, say "I'm allergic to cats"
-else if (pettype == "Cat" | "cat") alert("I'm allergic but they keep breaking into my house.  But on the bright side no more mice!");
+else if (pettype == "Cat" || "cat") alert("I'm allergic but they keep breaking into my house.  But on the bright side no more mice!");
 // If it is not a dog or a cat, say "what an interesting pet!"
 else alert("wow cool!  I heard those things can actually speak human language!");
