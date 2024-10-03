15205d730b7d6d2b425af7e2ad99b3bd7759f297
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-03 16:12:44 +0000

    done

--- done.js
+++ done.js
@@ -82,3 +82,16 @@ var petname= prompt("whats their name?");
 confirm("so you have a " + pettype + " named " + petname);
 
 /* Prompt with Conditional */
+// make a variable for pet type, prompt user for it 
+var pettype= prompt("What type of pet do you have?");
+// make a variable for pet name, prompt user for it
+var petname= prompt ("What is their name?");
+// alert "You have a pet type named pet name 
+alert("You have a " + pettype + " nammed " + petname);
+// if pet is a dog, say "I like dogs, too!"
+if (pettype == "Dog" || "dog") alert("I like dogs but they keep stealing from the mysterious bone pile ");
+// if it is a cat, say "I'm allergic to cats"
+else if (pettype == "Cat" || "cat") alert("I'm allergic but they keep breaking into my house and eatting all the mice");
+// If it is not a dog or a cat, say "what an interesting pet!"
+else alert("wow cool! I heard thoose could learn human language!");
+alert("This one doesnt work and I dont know why");
