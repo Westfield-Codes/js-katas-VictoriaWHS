8795fa0fbecc87aafd97d284898aa64ab8310562
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-04 15:24:39 +0000

    done

--- done.js
+++ done.js
@@ -15,7 +15,11 @@ alert(color + "is a nice color!");
 /* Prompt */
 
 /* Prompt with Conditional */
-
+var color = prompt(" what is your favorite color");
+// if user enters "black" alert "Black is not really a color." 
+if (color == "black") alert("black is not really a color.");
+// otherwise alert color " is a nice color!"
+else alert(color + " is a nice color");
 
 /* AGE */
 /* AGE KATAS */
---
+++ script.js
@@ -0,0 +1,7 @@
+/* Var Alert Prompt Conditional */
+// make a string variable for color, prompt the user for their favorite
+var color = prompt(" what is your favorite color");
+// if user enters "black" alert "Black is not really a color." 
+if (color == "black") alert("black is not really a color.");
+// otherwise alert color " is a nice color!"
+else alert(color + " is a nice color");
